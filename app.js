9050bd89085ab5f32d949f847e03dedfdc5e2284
leeight <leeight@gmail.com>
2015-12-16 21:44:34 +0800

    两个节点之间连线的操作比较平滑了

--- app.js
+++ app.js
@@ -13,21 +13,40 @@
 
 var paper = Raphael('holder', 640, 480);
 
-// 鼠标点击下去时候的 circle
-var g_startNode;  // circle
-
-// 鼠标松开时候的 circle
-var g_endNode;    // circle
-
-// 需要画的线
-var g_line;       // path
+/**
+ * @constructor
+ * @param {number} n1 Node1's id.
+ * @param {number} n2 Node2's id.
+ */
+function Edge(n1, n2) {
+    this.n1 = n1;
+    this.n2 = n2;
+}
 
 function Node(paper, config) {
     this.paper = paper;
     this.rect = paper.rect(config.x, config.y,
         config.width, config.height, config.radius || 5);
-
     this.circles = this._initInputAndOutput(config);
+
+    /*
+    if (config.text) {
+        this.text = paper.text(config.x, config.y + config.height / 2,
+            config.text);
+    }
+    */
+
+    /**
+     * 入边，移动的时候，调整 Mx,y 的值
+     * @type {Array.<paths>}
+     */
+    this.inEdges = [];
+
+    /**
+     * 出变，移动的时候，调整 Lx,y 的值
+     * @type {Array.<paths>}
+     */
+    this.outEdges = [];
 
     var color = Raphael.getColor();
     this.group = paper.set();
@@ -39,40 +58,14 @@ function Node(paper, config) {
 
 Node.prototype._initInputAndOutput = function (config) {
     function enlargeCircle(e) {
-        this.or = this.attr('r');
-        this.attr({r: 8});
+        this.attr({fill: 'yellow', r: 8});
     }
 
     function restoreCircleSize(e) {
-        if (this.or) {
-            this.attr({r: this.or});
-        }
+        this.attr({fill: 'white', r: 5});
     }
 
-    var node = this;
-    var paper = node.paper;
-
-    function startOfDrawLine() {
-        if (g_line) {
-            return;
-        }
-
-        // this -> paper.circle
-        g_startNode = this;
-
-        var x = this.attr('cx');
-        var y = this.attr('cy');
-        var start = 'M' + x + ' ' + y;
-        var path = start + 'L' + (x + 10) + ' ' + (y + 10);
-        g_line = paper.path(path);
-        g_line.start = start;
-    }
-
-    function endOfDrawLine() {
-        if (g_startNode !== this) {
-            g_endNode = this;
-        }
-    }
+    var refId = this.rect.id;
 
     var circles = [];
     if (config.input) {
@@ -83,8 +76,9 @@ Node.prototype._initInputAndOutput = function (config) {
             var circle = this.paper.circle(x, y, 5);
             circle.mouseover(enlargeCircle);
             circle.mouseout(restoreCircleSize);
-            circle.mousedown(startOfDrawLine);
-            circle.mouseup(endOfDrawLine);
+            // circle.mousedown(startOfDrawLine);
+            // circle.mouseup(endOfDrawLine);
+            circle.refId = refId;
             circles.push(circle);
         }
     }
@@ -96,8 +90,9 @@ Node.prototype._initInputAndOutput = function (config) {
             var circle = this.paper.circle(x, y, 5);
             circle.mouseover(enlargeCircle);
             circle.mouseout(restoreCircleSize);
-            circle.mousedown(startOfDrawLine);
-            circle.mouseup(endOfDrawLine);
+            // circle.mousedown(startOfDrawLine);
+            // circle.mouseup(endOfDrawLine);
+            circle.refId = refId;
             circles.push(circle);
         }
     }
@@ -159,24 +154,84 @@ var node2 = new Node(paper, {
 });
 node2.moveable();
 
-document.querySelector('#holder > svg').onmousemove = function (e) {
+// 鼠标点击下去时候的 circle
+var g_startNode;  // circle
+
+// 鼠标松开时候的 circle
+var g_endNode;    // circle
+
+// 需要画的线
+var g_line;       // path
+
+var canvas = document.querySelector('#holder > svg')
+canvas.onmousedown = function (e) {
+    var target = e.target;
+    if (target.nodeName !== 'circle'
+        || !target.raphael) {
+        return;
+    }
+
+    g_startNode = paper.getById(target.raphaelid);
+
+    var x = g_startNode.attr('cx');
+    var y = g_startNode.attr('cy');
+    var start = 'M' + x + ' ' + y;
+    var path = start + 'L' + (x + 1) + ' ' + (y + 1);
+    g_line = paper.path(path);
+    g_line.attr({'stroke-width': 2, 'stroke': '#cfcfcf', 'arrow-end': 'diamond'});
+    g_line.start = start;
+};
+
+canvas.onmouseover = function (e) {
+    var target = e.target;
+    if (target.nodeName !== 'circle'
+        || !target.raphael
+        || !g_startNode) {
+        return;
+    }
+
+    if (g_endNode && g_endNode.id === target.raphaelid) {
+        return;
+    }
+
+    var node = paper.getById(target.raphaelid);
+    if (g_startNode !== node) {
+        g_endNode = node;
+    }
+};
+
+canvas.onmousemove = function (e) {
     if (!g_line) {
         return;
     }
 
     var path = g_line.start;
-    if (g_endNode) {
-        var x = g_endNode.attr('cx');
-        var y = g_endNode.attr('cy');
-        g_line.attr('path', path + ' L' + x + ' ' + y);
-        g_line = null;
-        g_endNode = null;
+    path += ' L' + e.offsetX + ' ' + e.offsetY;
+    g_line.attr('path', path);
+};
+
+canvas.onmouseup = function (e) {
+    if (!g_endNode) {
+        if (g_line) {
+            g_line.remove();
+            g_line = null;
+            g_startNode = null;
+        }
         return;
     }
 
-    path += ' L' + e.offsetX + ' ' + e.offsetY;
-    g_line.attr({'arrow-end': 'classic', 'stroke-width': 2,
-        'stroke': '#cfcfcf', 'path': path});
+    if (!g_line) {
+        return;
+    }
+
+    var path = g_line.start;
+    var x = g_endNode.attr('cx');
+    var y = g_endNode.attr('cy');
+    g_line.attr('path', path + ' L' + x + ' ' + y);
+
+    g_line = null;
+    g_endNode = null;
+    g_startNode = null;
 };
 
 // var graph = new DirectedGraph();
